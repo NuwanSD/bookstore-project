10cce3922538b0ac08740c5ea031f9aa222f1907
Sagara Nuwan <sagaradnuwan@gmail.com>
2023-12-16 23:33:57 +0530

    Updated code

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,25 @@
-import express, { response } from "express";
+import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import cors from "cors";
+import { Book } from "./models/bookModel.js";
+import bookRoute from "./routes/booksRoute.js";
 
 const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
+
+// Use the cors middleware
+app.use(cors());
 
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("Welcome to the MERN stack project");
 });
+
+// Use book route functions here
+app.use("/books", bookRoute);
 
 mongoose
   .connect(mongoDBURL)
